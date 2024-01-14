b7e5d8906252221d6654a2e7b22cab77b44a9961
dev-shubhamsoni <dev.shubhamsoni@gmail.com>
2024-01-14 14:22:14 +0530

    made changes in api

--- client/src/apis/api_portfolio.js
+++ client/src/apis/api_portfolio.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 
 
-// const baseURL = "http://localhost:4000/api/v1"
-
-const baseURL =
-    process.env.NODE_ENV ===
-        "production" ? "api/v1" :
-        "http://localhost:4000/api/v1";
+// const baseURL = "http://localhost:4000/api/v1";
+const baseURL = "api/v1";
 
 const api = axios.create({
     baseURL,
