6cf108091cd6ceca4b57b0934ea1eeccf752e6a7
dev-shubhamsoni <dev.shubhamsoni@gmail.com>
2024-01-14 14:27:48 +0530

    update package

--- client/src/apis/api_portfolio.js
+++ client/src/apis/api_portfolio.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 
-const baseURL = "http://localhost:4000/api/v1";
-// const baseURL = "api/v1";
+// const baseURL = "http://localhost:4000/api/v1";
+const baseURL = "api/v1";
 
 const api = axios.create({
     baseURL,
